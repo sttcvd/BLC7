005c9f9c2d70eb35c1c76e013a4164421b0154c8
sttcvd <eshap06@gmail.com>
2025-05-21 23:28:02 -0500

    Random Number Generator Matrix. Pretend digital slot machine reel.

--- main.lua
+++ main.lua
@@ -1,13 +1,34 @@
 function love.load()
-    
+
     ht = love.graphics.getHeight()
     wdt = love.graphics.getWidth()
+    setMatrix(3, 5)
 end
 
 function love.update(dt)
-
+    
 end
+
+matrixOffset = 20 
 
 function love.draw()
-    love.graphics.print('777', wdt / 2, ht / 2)
+    drawReels(matrix)
 end
+
+matrix = {}
+
+function setMatrix(rows, columns)
+    for i = 1, rows, 1 do 
+        matrix[i] = {}
+        for j = 1, columns, 1 do
+            matrix[i][j] = love.math.random(1, 5)
+        end
+    end
+end
+
+function drawReels(roll)
+    for i = 1, #roll, 1 do
+        love.graphics.print(roll[i], wdt / 2, (ht / 2) - 50 + (i * 50), 0 , 5, 5, matrixOffset, matrixOffset)
+    end
+end
+
